4e527c540644abeff9b90104d91314786dffa00c
paolov1928 <ventura.paolo@gmail.com>
2018-12-03 19:38:14 +0000

    minor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :charities, only: [:index, :show]
-  resources :purchases, only: [:index, :show]
-  resources :investments, only: [:index, :show]
+  resources :charities, only: [:index, :show, :new]
+  resources :purchases, only: [:index, :show, :new]
+  resources :investments, only: [:index, :show, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_163530) do
+ActiveRecord::Schema.define(version: 2018_12_03_182946) do
+
+  create_table "charities", force: :cascade do |t|
+    t.text "name"
+    t.string "description"
+    t.integer "personoutreach"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "investments", force: :cascade do |t|
     t.text "name"
