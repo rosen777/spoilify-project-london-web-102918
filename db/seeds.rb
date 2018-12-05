367fd85bb6424bceff688744ce05829aedf6c438
paolov1928 <ventura.paolo@gmail.com>
2018-12-05 18:35:04 +0000

    added the seeds for purchases and imported css for cards on purchase page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+Investment.destroy_all
+Purchase.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -34,7 +37,10 @@ applehigh = Investment.create(name: "appleh", price: StockQuote::Stock.raw_quote
 # http://3.bp.blogspot.com/-gudqlEhcOp4/UOVKp_FH1YI/AAAAAAAAAy8/uRJTEllJ-Gk/s640/private-jet-interiors.jpg
 # £27,000,000
 
+# Might be an idea to sample the countrys
+
 toilet = Purchase.create(name: "Catellan Golden Toilet", price: 2527872, country: "Kuwait", imgsrc: "https://i.ytimg.com/vi/IWGj8CvH82E/maxresdefault.jpg")
 shoes = Purchase.create(name: "Balenciaga gym shoes", price: 595, country: "Spain", imgsrc: "https://assets.vogue.com/photos/5a3ae4b02d08ab213919613c/master/pass/00-promo-toe-shoes.jpg")
 watch = Purchase.create(name: "Montegrappa Chaos Golden", price: 52985, country: "Uzbekistan", imgsrc: "https://whichwatch.org/wp-content/uploads/2015/12/Montegrappa-Chaos-Gold-Watch.jpg")
 jet = Purchase.create(name: "Custom Boeing 747-8I", price: 27000000, country: "USA", imgsrc: "http://3.bp.blogspot.com/-gudqlEhcOp4/UOVKp_FH1YI/AAAAAAAAAy8/uRJTEllJ-Gk/s640/private-jet-interiors.jpg")
+jetpack = Purchase.create(name: "Jetsuit by Gravity Industries", price: 340000, country: "UK", imgsrc: "https://static.highsnobiety.com/wp-content/uploads/2018/07/27210700/gravity-industries-jet-suit-001-480x320.jpg")
