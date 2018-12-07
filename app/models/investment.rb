ff7738d26edb82477beab3a73ca890b1e8f4cc6d
rosen777 <rosen.toshev@flatironschool.com>
2018-12-07 13:29:43 +0000

    Charts and investment methods for each sector added

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -7,11 +7,15 @@ class Investment < ApplicationRecord
   # if you keep it in the gem output format you can call each one on it
 
   belongs_to :purchase
+  has_many :user_investments
+  has_many :users, through: :user_investments
 
   def self.biggest_investment
+    if Investment.all.select {|i| i.sector == UserInvestment.last.investment.sector }
     i = Investment.all.map{|i| [i.name[0..25], i.price]}[0..5]
       result = []
       result = i
+    end
   end
 
   def self.investment_energy
@@ -37,6 +41,19 @@ class Investment < ApplicationRecord
   arr = []
     top10_ordered.map do |s|
     arr = [ s["marketCap"], s["companyName"], s["latestPrice"] ]
+    end
+  end
+
+  def self.investment_energy3
+  url = "https://api.iextrading.com/1.0/stock/market/collection/sector?collectionName=Energy"
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+  array_of_hashes = response_hash.select { |m| m["marketCap"] != nil }
+  sorted_by_cap = array_of_hashes.sort_by {|t| t["marketCap"]}
+  top10_ordered = sorted_by_cap.reverse[0..4]
+  arr = []
+    top10_ordered.map do |s|
+    arr = [s["companyName"], s["latestPrice"] ]
     end
   end
 
@@ -66,6 +83,19 @@ end
   end
 end
 
+def self.investment_health_care3
+  url = "https://api.iextrading.com/1.0/stock/market/collection/sector?collectionName=Health%20Care"
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+  array_of_hashes = response_hash.select { |m| m["marketCap"] != nil }
+  sorted_by_cap = array_of_hashes.sort_by {|t| t["marketCap"]}
+  top10_ordered = sorted_by_cap.reverse[0..4]
+  arr= []
+  top10_ordered.map do |s| s["marketCap"]
+    arr = [ s["companyName"], s["latestPrice"] ]
+  end
+end
+
 def self.investment_technology
   url = "https://api.iextrading.com/1.0/stock/market/collection/sector?collectionName=Technology"
   response_string = RestClient.get(url)
@@ -89,6 +119,19 @@ def self.investment_technology2
   arr= []
   top10_ordered.map do |s| s["marketCap"]
   arr = [ s["marketCap"], s["companyName"], s["latestPrice"] ]
+end
+end
+
+def self.investment_technology3
+  url = "https://api.iextrading.com/1.0/stock/market/collection/sector?collectionName=Technology"
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+  array_of_hashes = response_hash.select { |m| m["marketCap"] != nil  }
+  sorted_by_cap = array_of_hashes.sort_by {|t| t["marketCap"]}
+  top10_ordered = sorted_by_cap.reverse[0..4]
+  arr= []
+  top10_ordered.map do |s| s["marketCap"]
+  arr = [ s["companyName"], s["latestPrice"] ]
 end
 end
 
@@ -118,6 +161,19 @@ def self.investment_consumer_defensive2
 end
 end
 
+def self.investment_consumer_defensive3
+  url = "https://api.iextrading.com/1.0/stock/market/collection/sector?collectionName=Consumer%20Defensive"
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+  array_of_hashes = response_hash.select { |m| m["marketCap"] != nil }
+  sorted_by_cap = array_of_hashes.sort_by {|t| t["marketCap"]}
+  top10_ordered = sorted_by_cap.reverse[0..4]
+  arr= []
+  top10_ordered.map do |s| s["marketCap"]
+  arr = [ s["companyName"], s["latestPrice"] ]
+end
+end
+
 def self.investment_consumer_cyclical
     url = "https://api.iextrading.com/1.0/stock/market/collection/sector?collectionName=Consumer%20Cyclical"
     response_string = RestClient.get(url)
@@ -143,5 +199,20 @@ def self.investment_consumer_cyclical2
     arr = [ s["marketCap"], s["companyName"], s["latestPrice"] ]
 end
 end
+
+def self.investment_consumer_cyclical3
+    url = "https://api.iextrading.com/1.0/stock/market/collection/sector?collectionName=Consumer%20Cyclical"
+    response_string = RestClient.get(url)
+    response_hash = JSON.parse(response_string)
+    array_of_hashes = response_hash.select { |m| m["marketCap"] != nil }
+    sorted_by_cap = array_of_hashes.sort_by {|t| t["marketCap"]}
+    top10_ordered = sorted_by_cap.reverse[0..4]
+    arr= []
+    top10_ordered.map do |s| s["marketCap"]
+    arr = [ s["companyName"], s["latestPrice"] ]
+end
+end
+
+
 
 end
